fa25f17cd5769b45f63ecf831f9e40b2a4befe06
alberto0267 <unnamed0267@gmail.com>
2025-06-02 11:09:57 +0200

    FecthConjunto

--- bebidasPinia/src/components/Receta.vue
+++ bebidasPinia/src/components/Receta.vue
@@ -8,7 +8,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="bg-white rounded shadow p-4">
+  <div class="bg-white rounded shadow p-4 cursor-pointer">
+    <img :src="receta.strDrinkThumb" alt="" />
     <h2 class="text-lg font-bold text-black">{{ receta.strDrink }}</h2>
   </div>
 </template>
--- bebidasPinia/src/views/InicioView.vue
+++ bebidasPinia/src/views/InicioView.vue
@@ -1,26 +1,14 @@
 <script setup>
 import { useBebidasStore } from '../stores/bebidas'
-import { storeToRefs } from 'pinia'
-import { watch } from 'vue'
+
 import Receta from '../components/Receta.vue'
 const bebidas = useBebidasStore()
-const { recetas } = storeToRefs(bebidas)
-
-// // 2. Extraemos la propiedad `recetas` como ref reactiva
-// const { recetas } = storeToRefs(bebidas)
-
-// // 3. Observamos si cambian las recetas
-// watch(recetas, () => {
-//   console.log('Recetas cargadas:', recetas.value)
-// })
-//
 </script>
 
 <template>
   <div>
     <h1 class="text-6xl font-extrabold">Recetas</h1>
-
-    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 gap-10 bg-red-500">
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 mt-10 gap-10">
       <Receta v-for="(receta, index) in bebidas.recetas" :receta="receta" :key="index" />
     </div>
   </div>
